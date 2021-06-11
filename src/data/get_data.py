4440ac40ac065c70db9654ae25f065c57aaf3a2c
MHThomsen <mortenthomsen4@gmail.com>
2021-06-11 15:06:02 +0200

    black formatting

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -8,30 +8,33 @@ import numpy as np
 
 
 @click.command()
-@click.option('--process_data', default=True, help='Apply  to the raw dataset')
+@click.option("--process_data", default=True, help="Apply  to the raw dataset")
 def main(process_data):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('Collecting the dataset')
+    logger.info("Collecting the dataset")
     # Assume data
-    if Path('./data/processed/train_set.csv').exists():
+    if Path("./data/processed/train_set.csv").exists():
         print("Data already downloaded.")
     else:
-        #find and load environment file
+        # find and load environment file
         load_dotenv(find_dotenv())
         import kaggle
-        #authenticates kaggle user using environment file
+
+        # authenticates kaggle user using environment file
         kaggle.api.authenticate()
-        #downloads and unzips to raw folder
-        
-        kaggle.api.dataset_download_files('team-ai/spam-text-message-classification', path = 'data/raw', unzip=True)
-        #time.sleep(1)
+        # downloads and unzips to raw folder
+
+        kaggle.api.dataset_download_files(
+            "team-ai/spam-text-message-classification", path="data/raw", unzip=True
+        )
+        # time.sleep(1)
         print("Succesfully downloaded kaggle data")
 
-    #split data into train/test/val
-    raw_data = pd.read_csv('data/raw/SPAM text message 20170820 - Data.csv')
+    # split data into train/test/val
+    raw_data = pd.read_csv("data/raw/SPAM text message 20170820 - Data.csv")
     np.random.seed(404)
     msk = np.random.rand(len(raw_data)) < 0.8
     msk_ = np.random.rand(len(raw_data[msk])) < 0.8
@@ -40,14 +43,13 @@ def main(process_data):
     val_data = raw_data[msk][~msk_]
     test_data = raw_data[~msk]
 
-    train_data.to_csv('data/processed/train_set.csv')
-    val_data.to_csv('data/processed/val_set.csv')
-    test_data.to_csv('data/processed/test_set.csv')
-    
+    train_data.to_csv("data/processed/train_set.csv")
+    val_data.to_csv("data/processed/val_set.csv")
+    test_data.to_csv("data/processed/test_set.csv")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
@@ -55,6 +57,6 @@ if __name__ == '__main__':
 
     # find .env automagically by walking up directories until it's found, then
     # load up the .env entries as environment variables
-    #load_dotenv(find_dotenv())
+    # load_dotenv(find_dotenv())
 
     main()
--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -8,30 +8,43 @@ from transformers import AutoTokenizer
 
 from src.models.model import LSTM
 
+
 def test_check_data_size():
     assert True
 
 
-#Testing if model output size matches the expected size
-def model_output(vocab_size,text_len,batch):
+# Testing if model output size matches the expected size
+def model_output(vocab_size, text_len, batch):
 
-    model = LSTM(vocab_size,hidden_dimension=32,embedding_dim=10,text_len=text_len) 
-    
-    return model(batch['input_ids'])
+    model = LSTM(vocab_size, hidden_dimension=32, embedding_dim=10, text_len=text_len)
+
+    return model(batch["input_ids"])
+
 
 def test_model_dim():
-    
-    input_dim = 40
-    tokenizer = AutoTokenizer.from_pretrained('bert-base-cased')
-    
-    test_texts = ["Go until jurong point, crazy.. Available only in bugis n great world la e buffet... Cine there got amore wat...",
-    "Ok lar... Joking wif u oni...",
-    "Free entry in 2 a wkly comp to win FA Cup final tkts 21st May 2005. Text FA to 87121 to receive entry question(std txt rate)T&C's apply 08452810075over18's",
-    "U dun say so early hor... U c already then say...",
-    "Nah I don't think he goes to usf, he lives around here though",
-    "FreeMsg Hey there darling it's been 3 week's now and no word back! I'd like some fun you up for it still? Tb ok! XxX std chgs to send, £1.50 to rcv",]
 
-    batch = tokenizer(test_texts, padding='max_length', truncation=True, max_length=input_dim,add_special_tokens=False,return_tensors="pt")
-  
-    
-    assert model_output(tokenizer.vocab_size,input_dim,batch).shape == (len(test_texts),1)
+    input_dim = 40
+    tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+    test_texts = [
+        "Go until jurong point, crazy.. Available only in bugis n great world la e buffet... Cine there got amore wat...",
+        "Ok lar... Joking wif u oni...",
+        "Free entry in 2 a wkly comp to win FA Cup final tkts 21st May 2005. Text FA to 87121 to receive entry question(std txt rate)T&C's apply 08452810075over18's",
+        "U dun say so early hor... U c already then say...",
+        "Nah I don't think he goes to usf, he lives around here though",
+        "FreeMsg Hey there darling it's been 3 week's now and no word back! I'd like some fun you up for it still? Tb ok! XxX std chgs to send, £1.50 to rcv",
+    ]
+
+    batch = tokenizer(
+        test_texts,
+        padding="max_length",
+        truncation=True,
+        max_length=input_dim,
+        add_special_tokens=False,
+        return_tensors="pt",
+    )
+
+    assert model_output(tokenizer.vocab_size, input_dim, batch).shape == (
+        len(test_texts),
+        1,
+    )
