dca2f332f03cf7e34d14b2fb97250d5406a0d295
MHThomsen <mortenthomsen4@gmail.com>
2021-06-17 08:32:04 +0200

    OG training loopready

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,17 +3,25 @@ import transformers
 import wandb
 import sys
 import torch
-import tqdm
+
+
+import numpy as np
 
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
+import pytorch_lightning as pl
 
-
+from sklearn.metrics import accuracy_score
+from tqdm import tqdm
 from transformers import AutoTokenizer
+
 from src.models.model import LSTM
+from src.data.data_utils import SPAMorHAMDataset
+from src.models.lightning import lynModel
 
 
-wandb.init()
+# wandb.init()
 
 
 class TrainOREvaluate(object):
@@ -70,7 +78,7 @@ class TrainOREvaluate(object):
         parser.add_argument(
             "--hidden_dim",
             "--hidden-dim",
-            "-h",
+            "-hd",
             default=128,
             type=int,
             help="Dimension of hidden layer in LSTM network",
@@ -89,10 +97,16 @@ class TrainOREvaluate(object):
         # Text preprocessor
         tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
 
-        train_set = MNIST(
-            "./data", train=True, download=False, transform=self.transform
+        train_set = SPAMorHAMDataset(
+            "/Users/morten/Desktop/DTU/ML_Ops/SpamClassification/data/processed/test_set.csv",
+            input_dim=args.token_len,
+            tokenizer=tokenizer,
         )
-        train_set, val_set = torch.utils.data.random_split(train_set, [50000, 10000])
+        val_set = SPAMorHAMDataset(
+            "/Users/morten/Desktop/DTU/ML_Ops/SpamClassification/data/processed/val_set.csv",
+            input_dim=args.token_len,
+            tokenizer=tokenizer,
+        )
 
         trainloader = torch.utils.data.DataLoader(
             train_set, batch_size=64, shuffle=True
@@ -108,7 +122,7 @@ class TrainOREvaluate(object):
         )
 
         # Magic
-        wandb.watch(model, log_freq=100)
+        # wandb.watch(model, log_freq=100)
 
         criterion = nn.BCELoss()
         optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
@@ -130,9 +144,12 @@ class TrainOREvaluate(object):
             train_loss = 0
             train_iters = 0
 
-            for train_images, train_labels in trainiter:
+            for elem in trainiter:
+                train_messages = elem["message"]
+                train_labels = elem["label"]
+
                 optimizer.zero_grad()
-                output = model(train_images)
+                output = model(train_messages)
                 batch_loss = criterion(output, train_labels)
                 batch_loss.backward()
 
@@ -143,7 +160,7 @@ class TrainOREvaluate(object):
                 # writer.add_scalar('Loss/train', batch_loss, writer_train_iter)
                 # writer_train_iter+=1
 
-                wandb.log({"train_loss": batch_loss})
+                # wandb.log({"train_loss": batch_loss})
 
             train_loss = train_loss / train_iters
             train_losses[epoch] = train_loss
@@ -155,13 +172,16 @@ class TrainOREvaluate(object):
 
             # Accuracy measures
             val_preds, val_targs = [], []
-            for valid_images, valid_labels in valiter:
-                output = model(valid_images)
+            for elem in valiter:
+                valid_messages = elem["message"]
+                valid_labels = elem["label"]
+
+                output = model(valid_messages)
                 val_batch_loss = criterion(output, valid_labels)
                 val_loss += val_batch_loss
-
-                # Softmax max arg
-                preds = torch.max(output, 1)[1]
+                # Class predictions from model
+                preds = torch.zeros(output.shape)
+                preds[output >= 0.5] = 1
 
                 val_targs += list(valid_labels.numpy())
                 val_preds += list(preds.data.numpy())
@@ -170,7 +190,7 @@ class TrainOREvaluate(object):
                 # writer.add_scalar('Loss/val', val_batch_loss, writer_val_iter)
                 # writer_val_iter+=1
 
-                wandb.log({"val_loss": val_batch_loss})
+                # wandb.log({"val_loss": val_batch_loss})
 
             valid_acc_cur = accuracy_score(val_targs, val_preds)
 
@@ -187,15 +207,16 @@ class TrainOREvaluate(object):
         # writer.close()
 
         epoch = np.arange(num_epochs)
+
         plt.figure()
         plt.plot(epoch, train_losses, "r", epoch, valid_losses, "b")
         plt.legend(["Train Loss", "Validation Loss"])
         plt.xlabel("Updates"), plt.ylabel("Loss")
         plt.show()
-        plt.savefig("./reports/figures")
-        torch.save(
-            model, "./models/Net/net_{}.model".format(len(os.listdir("models")))
-        )  # creates subfolders
+        # plt.savefig("./reports/figures")
+        # torch.save(
+        #    model, "./models/Net/net_{}.model".format(len(os.listdir("models")))
+        # )  # creates subfolders
 
     def evaluate(self):
         print("Evaluating until hitting the ceiling")
@@ -231,6 +252,89 @@ class TrainOREvaluate(object):
             test_preds += list(preds.data.numpy())
 
         print("Model test accuracy:", accuracy_score(test_targs, test_preds))
+
+    def lightning(self):
+        print("Training day and night")
+        parser = argparse.ArgumentParser(description="Training arguments")
+        parser.add_argument(
+            "--lr", default=0.01, type=float, help="Learning rate for optimizer"
+        )
+        parser.add_argument(
+            "--epoch",
+            "-ep",
+            default=2,
+            type=int,
+            help="Numper of epochs in training loop",
+        )
+        parser.add_argument(
+            "--token_len",
+            "--token_len",
+            "-t",
+            default=40,
+            type=int,
+            help="Max numbers of tokens in each string (pad or truncate to len)",
+        )
+        parser.add_argument(
+            "--embedding_dim",
+            "--embedding-dim",
+            "-em",
+            default=20,
+            type=int,
+            help="Dimension of embedding layer in LSTM",
+        )
+        parser.add_argument(
+            "--hidden_dim",
+            "--hidden-dim",
+            "-hd",
+            default=128,
+            type=int,
+            help="Dimension of hidden layer in LSTM network",
+        )
+        parser.add_argument(
+            "--dropout",
+            "-d",
+            default=0.5,
+            type=float,
+            help="Dropout rate for between LSTM layer and hidden layer",
+        )
+        # add any additional argument that you want
+        args = parser.parse_args(sys.argv[2:])
+        print(args)
+        # Text preprocessor
+        tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+        train_set = SPAMorHAMDataset(
+            "./data/processed/test_set.csv",
+            input_dim=args.token_len,
+            tokenizer=tokenizer,
+        )
+        val_set = SPAMorHAMDataset(
+            "./data/processed/val_set.csv",
+            input_dim=args.token_len,
+            tokenizer=tokenizer,
+        )
+
+        trainloader = torch.utils.data.DataLoader(
+            train_set,
+            batch_size=128,
+            shuffle=True,
+        )
+        validloader = torch.utils.data.DataLoader(
+            val_set, batch_size=128, shuffle=False
+        )
+
+        # Model
+        model = LSTM(
+            tokenizer.vocab_size,
+            hidden_dimension=args.hidden_dim,
+            embedding_dim=args.embedding_dim,
+            text_len=args.token_len,
+        )
+
+        trainer = pl.Trainer(max_epochs=args.epoch)
+        litmodel = lynModel(model, optimizer_lr=args.lr)
+
+        trainer.fit(litmodel, trainloader, validloader)
 
 
 if __name__ == "__main__":
