1f76711542f36ca9702873b244261c66f9a6bb40
MHThomsen <mortenthomsen4@gmail.com>
2021-06-23 14:23:28 +0200

    new dataset required for datadrift to work

--- src/data/data_utils.py
+++ src/data/data_utils.py
@@ -39,3 +39,40 @@ class SPAMorHAMDataset(Dataset):
 
         sample = {"message": message.squeeze(), "label": label}
         return sample
+
+
+class SPAMorHAMDriftset(Dataset):
+    """SPAM or HAM data"""
+
+    def __init__(self, csv_file, input_dim, tokenizer=None):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.frame = pd.read_csv(csv_file)
+        self.tokenizer = tokenizer
+        self.max_length = input_dim
+
+    def __len__(self):
+        return len(self.frame)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        message = [self.frame.iloc[idx, 1]]
+        label = torch.Tensor([self.frame.iloc[idx, 0]])
+
+        if self.tokenizer:
+            message = self.tokenizer(
+                message,
+                padding="max_length",
+                truncation=True,
+                max_length=self.max_length,
+                add_special_tokens=False,
+                return_tensors="pt",
+            )["input_ids"]
+
+        return message.squeeze()
